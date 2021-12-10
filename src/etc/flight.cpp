50b9a64ab05c1188b2aa63a1ba76f00d3d2baac3
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2021-12-10 23:46:27 +0330

    implement flight

--- src/etc/flight.cpp
+++ src/etc/flight.cpp
@@ -1,1 +1,49 @@
 #include "flight.h"
+
+flight::flight(const string &flightSerial, const string &planeSerial, const string &origin, const string &dest,
+               const string &flightDate, const string &flightTime, int numOfPassengers, const pilot &pilot,
+               const vector<host> &hosts, double ticketsIncome) : flightSerial(flightSerial), planeSerial(planeSerial),
+                                                                  origin(origin), dest(dest), flightDate(flightDate),
+                                                                  flightTime(flightTime),
+                                                                  numOfPassengers(numOfPassengers), pilot(pilot),
+                                                                  hosts(hosts), ticketsIncome(ticketsIncome) {}
+
+const string &flight::getFlightSerial() const {
+    return flightSerial;
+}
+
+const string &flight::getPlaneSerial() const {
+    return planeSerial;
+}
+
+const string &flight::getOrigin() const {
+    return origin;
+}
+
+const string &flight::getDest() const {
+    return dest;
+}
+
+const string &flight::getFlightDate() const {
+    return flightDate;
+}
+
+const string &flight::getFlightTime() const {
+    return flightTime;
+}
+
+int flight::getNumOfPassengers() const {
+    return numOfPassengers;
+}
+
+const pilot &flight::getPilot() const {
+    return pilot;
+}
+
+const vector<host> &flight::getHosts() const {
+    return hosts;
+}
+
+double flight::getTicketsIncome() const {
+    return ticketsIncome;
+}
--- src/etc/flight.h
+++ src/etc/flight.h
@@ -1,8 +1,50 @@
 #ifndef AIRPORTMANAGEMENTSYSTEM_FLIGHT_H
 #define AIRPORTMANAGEMENTSYSTEM_FLIGHT_H
 
+#include "string"
+#include "vector"
+#include "../people/pilot.h"
+#include "../people/host.h"
+
+using namespace std;
 
 class flight {
+private:
+    string flightSerial;
+    string planeSerial;
+    string origin;
+    string dest;
+    string flightDate;
+    string flightTime;
+    int numOfPassengers;
+    pilot pilot;
+    vector<host> hosts;
+    double ticketsIncome;
+
+public:
+    flight(const string &flightSerial, const string &planeSerial, const string &origin, const string &dest,
+           const string &flightDate, const string &flightTime, int numOfPassengers, const pilot &pilot,
+           const vector<host> &hosts, double ticketsIncome);
+
+    const string &getFlightSerial() const;
+
+    const string &getPlaneSerial() const;
+
+    const string &getOrigin() const;
+
+    const string &getDest() const;
+
+    const string &getFlightDate() const;
+
+    const string &getFlightTime() const;
+
+    int getNumOfPassengers() const;
+
+    const pilot &getPilot() const;
+
+    const vector<host> &getHosts() const;
+
+    double getTicketsIncome() const;
 
 };
 
