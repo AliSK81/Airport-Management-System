18de7d2d14624c8f2c6a075b464d3f1000eb8314
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2021-12-10 23:46:38 +0330

    implement ticket

--- src/etc/ticket.cpp
+++ src/etc/ticket.cpp
@@ -1,1 +1,63 @@
 #include "ticket.h"
+
+ticket::ticket(const string &flightSerial, const string &flightDate, const string &flightTime, const string &origin,
+               const string &dest, const string &passengerName, const string &passengerFamily,
+               const string &passengerId, const string &planeSerial, const string &pilotPId, const string &carrierId,
+               double ticketPrice, double profitRate) : flightSerial(flightSerial), flightDate(flightDate),
+                                                        flightTime(flightTime), origin(origin), dest(dest),
+                                                        passengerName(passengerName), passengerFamily(passengerFamily),
+                                                        passengerId(passengerId), planeSerial(planeSerial),
+                                                        pilotPId(pilotPId), carrierId(carrierId),
+                                                        ticketPrice(ticketPrice), profitRate(profitRate) {}
+
+const string &ticket::getFlightSerial() const {
+    return flightSerial;
+}
+
+const string &ticket::getFlightDate() const {
+    return flightDate;
+}
+
+const string &ticket::getFlightTime() const {
+    return flightTime;
+}
+
+const string &ticket::getOrigin() const {
+    return origin;
+}
+
+const string &ticket::getDest() const {
+    return dest;
+}
+
+const string &ticket::getPassengerName() const {
+    return passengerName;
+}
+
+const string &ticket::getPassengerFamily() const {
+    return passengerFamily;
+}
+
+const string &ticket::getPassengerId() const {
+    return passengerId;
+}
+
+const string &ticket::getPlaneSerial() const {
+    return planeSerial;
+}
+
+const string &ticket::getPilotPId() const {
+    return pilotPId;
+}
+
+const string &ticket::getCarrierId() const {
+    return carrierId;
+}
+
+double ticket::getTicketPrice() const {
+    return ticketPrice;
+}
+
+double ticket::getProfitRate() const {
+    return profitRate;
+}
